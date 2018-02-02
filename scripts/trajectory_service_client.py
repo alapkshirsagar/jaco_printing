6615cda289b9b07943ab9148140953a84525e474
alapkshirsagar <alapkshirsagar@gmail.com>
2018-02-02 17:27:59 -0500

    added listener for windows PC communication

--- scripts/trajectory_service_client.py
+++ scripts/trajectory_service_client.py
@@ -4,6 +4,7 @@ import sys
 import rospy
 from kinova_msgs.srv import *
 from kinova_msgs.msg import *
+from std_msgs.msg import String
 
 """ Global variable """
 currentCartesianCommand = [0.212322831154, -0.257197618484, 0.509646713734, 1.63771402836, 1.11316478252, 0.134094119072] # default home in unit mq
@@ -46,30 +47,47 @@ def setcurrentCartesianCommand(feedback):
     # currentCartesianCommand = [feedback.X, feedback.Y, feedback.Z, feedback.ThetaX, feedback.ThetaY, feedback.Z]
     # print 'currentCartesianCommand in setcurrentCartesianCommand is: ', currentCartesianCommand
 
+def windowsSubscriber():
+    topic_address = '/chatter'
+    rospy.Subscriber(topic_address,String,call_trajectory_client)
+    rospy.spin()
+
+def call_trajectory_client(feedback):
+    fields = feedback.data.split(',')
+    targetPose.X = currentCartesianCommand[0]+ float(fields[0])
+    targetPose.Y = currentCartesianCommand[1]+ float(fields[1])
+    targetPose.Z = currentCartesianCommand[2]+ float(fields[2])
+    targetPose.ThetaX = currentCartesianCommand[3]+ float(fields[3])
+    targetPose.ThetaY = currentCartesianCommand[4]+ float(fields[4])
+    targetPose.ThetaZ = currentCartesianCommand[5]+ float(fields[5])
+    print "Requesting to add point to trajectory"
+    add_pose_to_Cartesian_trajectory_client(targetPose)
+
 
 
 if __name__ == "__main__":
     rospy.init_node('trajectory_service_client')
-    getcurrentCartesianCommand()
-    print currentCartesianCommand
+    #getcurrentCartesianCommand()
+    #print currentCartesianCommand
 
-    print "Requesting to clear trajectories"
-    clear_trajectories_client()
+    #print "Requesting to clear trajectories"
+    #clear_trajectories_client()
 
-    targetPose = KinovaPose()
-    file = open('poses.txt')
-    for line in file:
-        fields = line.strip().split()
-        print fields[0], fields[1], fields[2], fields[3], fields[4], fields[5]
-        targetPose.X = currentCartesianCommand[0]+ float(fields[0])
-        targetPose.Y = currentCartesianCommand[1]+ float(fields[1])
-        targetPose.Z = currentCartesianCommand[2]+ float(fields[2])
-        targetPose.ThetaX = currentCartesianCommand[3]+ float(fields[3])
-        targetPose.ThetaY = currentCartesianCommand[4]+ float(fields[4])
-        targetPose.ThetaZ = currentCartesianCommand[5]+ float(fields[5])
-        print "Requesting to add point to trajectory"
-        add_pose_to_Cartesian_trajectory_client(targetPose)
-        #
+    windowsSubscriber()
+    # targetPose = KinovaPose()
+    # file = open('poses.txt')
+    # for line in file:
+    #     fields = line.strip().split()
+    #     print fields[0], fields[1], fields[2], fields[3], fields[4], fields[5]
+    #     targetPose.X = currentCartesianCommand[0]+ float(fields[0])
+    #     targetPose.Y = currentCartesianCommand[1]+ float(fields[1])
+    #     targetPose.Z = currentCartesianCommand[2]+ float(fields[2])
+    #     targetPose.ThetaX = currentCartesianCommand[3]+ float(fields[3])
+    #     targetPose.ThetaY = currentCartesianCommand[4]+ float(fields[4])
+    #     targetPose.ThetaZ = currentCartesianCommand[5]+ float(fields[5])
+    #     print "Requesting to add point to trajectory"
+    #     add_pose_to_Cartesian_trajectory_client(targetPose)
+    #     #
     # targetPose.X = 0.21
     # targetPose.Y = -0.25
     # targetPose.Z = 0.51
